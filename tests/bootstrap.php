314d461dd7ba719f77644f0101488434e6804e0f
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-08-10 09:02:35 +0000

    Test.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+die(var_dump($_ENV));
+
 if (!file_exists(__DIR__ . '/config.php')) {
     throw new \Exception('Cannot run tests without test app credentials. Rename config.php.dist to config.php.');
 }
